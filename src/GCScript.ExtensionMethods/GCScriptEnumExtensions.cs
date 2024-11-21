4ece7c07455c22cd8ca647c883becb8be2d56447
Gustavo Costa <gcs5stars@gmail.com>
2024-11-21 10:57:12 -0300

    Create GCScriptEnumExtensions.cs

---
+++ src/GCScript.ExtensionMethods/GCScriptEnumExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace GCScript.ExtensionMethods;
+public static class GCScriptEnumExtensions {
+	public static string GetDescription(this Enum value) {
+		var field = value.GetType().GetField(value.ToString());
+		var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+		return attribute?.Description ?? value.ToString();
+	}
+}
